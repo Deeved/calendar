e89532a555f0d78f527a29d2db11177c9fdee822
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-08-15 15:16:50 -0300

    adding class Day

---
+++ src/app/Helpers/Day.ts
@@ -0,0 +1,96 @@
+export class Day {
+  January = 0;
+  FirstDayOfTheYear = 1;
+  dayInMilliseconds = 86400000;
+  totalDaysOfTheWeek = 7;
+
+  constructor(private date: Date = null, private lang = "default") {
+    date ? (this.date = date) : (this.date = new Date());
+  }
+
+  get dayOfTheMonth() {
+    // getdate
+    return this.date.getDate();
+  }
+
+  get dayOfTheWeekInNumeral() {
+    return this.date.getDay() + 1; // dayNumber
+  }
+
+  get dayOfTheWeekInLongString() {
+    return this.date.toLocaleString(this.lang, { weekday: "long" }); // day
+  }
+
+  get dayOfTheWeekInShortString() {
+    return this.date.toLocaleString(this.lang, { weekday: "short" }); // dayShort
+  }
+
+  get monthInLongString() {
+    return this.date.toLocaleString(this.lang, { month: "long" }); //month
+  }
+
+  get monthInShortString() {
+    return this.date.toLocaleString(this.lang, { month: "short" });
+  }
+
+  get monthInNumeral() {
+    return Number(this.date.toLocaleString(this.lang, { month: "2-digit" }));
+  }
+
+  get year() {
+    return this.date.getFullYear();
+  }
+
+  get yearShort() {
+    return Number(this.date.toLocaleString(this.lang, { year: "2-digit" }));
+  }
+
+  get timestamp() {
+    return this.date.getTime();
+  }
+
+  get weekNumber() {
+    const firstDayOfTheYear = new Date(
+      this.year,
+      this.January,
+      this.FirstDayOfTheYear
+    );
+    const pastDaysOfYear =
+      (this.timestamp - firstDayOfTheYear.getTime()) / this.dayInMilliseconds;
+
+    return Math.ceil(
+      (pastDaysOfYear + firstDayOfTheYear.getDay() + 1) /
+        this.totalDaysOfTheWeek
+    );
+  }
+
+  get isToday() {
+    return this.isEqualTo(new Date());
+  }
+
+  isEqualTo(date: Date) {
+    date = date instanceof Day ? date.date : date;
+
+    return (
+      date.getDate() === this.date.getDate() &&
+      date.getMonth() === this.monthInNumeral - 1 &&
+      date.getFullYear() === this.year
+    );
+  }
+
+  format(formatStr) {
+    return formatStr
+      .replace(/\bYYYY\b/, this.year)
+      .replace(/\bYYY\b/, this.yearShort)
+      .replace(/\bWW\b/, this.weekNumber.toString().padStart(2, "0"))
+      .replace(/\bW\b/, this.weekNumber)
+      .replace(/\bDDDD\b/, this.dayOfTheWeekInLongString)
+      .replace(/\bDDD\b/, this.dayOfTheWeekInShortString)
+      .replace(/\bDD\b/, this.dayOfTheMonth.toString().padStart(2, "0"))
+      .replace(/\bD\b/, this.date)
+      .replace(/\bMMMM\b/, this.monthInLongString)
+      .replace(/\bMMM\b/, this.monthInShortString)
+      .replace(/\bMM\b/, this.monthInNumeral.toString().padStart(2, "0"))
+      .replace(/\bM\b/, this.monthInNumeral);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Day } from "./Helpers/Day";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'design-system';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const day = new Day();
+    console.log("-- getDayOfTheMonth", day.dayOfTheMonth);
+    console.log("-- getDayOfTheWeekInNumeral", day.dayOfTheWeekInNumeral);
+    console.log(
+      "-- getDayOfTheWeekInShortString",
+      day.dayOfTheWeekInShortString
+    );
+    console.log("-- getDayOfTheWeekInLongString", day.dayOfTheWeekInLongString);
+    console.log("-- yearShort", day.yearShort);
+    console.log("-- year", day.year);
+    console.log("-- month", day.monthInShortString);
+    console.log("-- month", day.monthInLongString);
+    console.log("-- month", day.monthInNumeral);
+    console.log("-- timestamp", day.timestamp);
+    console.log("-- weekNumber", day.weekNumber);
+    console.log("-- isToday", day.isToday);
+    console.log("--  day", day.format("MMM DD (DDDD/WW), YYYY"));
+  }
 }
