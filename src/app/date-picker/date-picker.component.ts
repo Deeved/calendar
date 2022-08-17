97f3f3eb024d287037cceb5273c65587e8c11555
Deeved Hiuston <deeved.hiuston@grupobrisanet.com.br>
2022-08-17 13:05:50 -0300

    feat: adding days in calendar

--- src/app/date-picker/date-picker.component.ts
+++ src/app/date-picker/date-picker.component.ts
@@ -15,34 +15,39 @@ export class DatePickerComponent implements OnInit {
   date: Day;
   monthYear: string;
   calendar: Calendar;
-  lang: string = null
+  lang: string = null;
   calendarElement: HTMLElement;
+  selectedDayElement;
 
   constructor() {
     this.lang = window.navigator.language;
   }
 
   toggle() {
-    if(!this.visible) {
-      this.isCurrentCalendarMonth()
+    if (!this.visible) {
+      this.isCurrentCalendarMonth();
 
       this.calendar.goToDate(this.date.monthNumber, this.date.year);
-      this.updatedMonthYear();      
+      this.updatedMonthYear();
     }
 
     this.visible = !this.visible;
   }
 
   ngOnInit() {
-    const date = this.dateLabel ? new Date(this.dateLabel) : new Date();  
-    
+    const date = this.dateLabel ? new Date(this.dateLabel) : new Date();
+
     this.date = new Day(date, this.lang);
-    
-    this.calendar = new Calendar(this.date.year, this.date.monthNumber, this.lang);
-        
+
+    this.calendar = new Calendar(
+      this.date.year,
+      this.date.monthNumber,
+      this.lang
+    );
+
     this.dateLabel = this.date.format(this.format);
-    
-    this.monthYear = `${this.calendar.month.name}, ${this.calendar.year}`; 
+
+    this.monthYear = `${this.calendar.month.name}, ${this.calendar.year}`;
 
     // document.addEventListener('click', (e) => this.handleClickOut(e))
     // const calendar = document.getElementById('calendar');
@@ -51,61 +56,116 @@ export class DatePickerComponent implements OnInit {
     //   this.calendarElement = calendar
     //   this.calendarElement.addEventListener('click', (e) => this.handleClickOut(e))
     // }
-    
+    this.updateMonthDays();
   }
 
   previousMonth() {
-    this.calendar.goToPreviousMonth()
-    this.updatedMonthYear()
+    this.calendar.goToPreviousMonth();
+    this.updatedMonthYear();
   }
 
   nextMonth() {
-    this.calendar.goToNextMonth()
-    this.updatedMonthYear()
+    this.calendar.goToNextMonth();
+    this.updatedMonthYear();
   }
 
   updatedMonthYear() {
-    this.monthYear = `${this.calendar.month.name}, ${this.calendar.year}`
+    this.monthYear = `${this.calendar.month.name}, ${this.calendar.year}`;
   }
-  
+
   isCurrentCalendarMonth() {
-    return this.calendar.month.number === this.date.monthNumber && 
+    return (
+      this.calendar.month.number === this.date.monthNumber &&
       this.calendar.year === this.date.year
+    );
   }
 
   handleClickOut(e) {
     console.log(this);
-    
+
     // console.log(e.target);
-  } 
-  
+  }
+
   getWeekDaysElementStrings() {
-    return this.calendar.weekDays
-      .map((weekDay) => `${(weekDay as string).substring(0, 3)}`)
+    return this.calendar.weekDays.map(
+      (weekDay) => `${(weekDay as string).substring(0, 3)}`
+    );
   }
 
   getMonthDaysGrid() {
     const firstDayOfTheMonth = this.calendar.month.getDay(1);
-    const totalLastMonthFinalDays = firstDayOfTheMonth.dayNumber - 1
-    const totalDays = this.calendar.month.numberOfDays + totalLastMonthFinalDays;
-    const monthList = Array.from({length: totalDays});
+    const totalLastMonthFinalDays = firstDayOfTheMonth.dayNumber - 1;
+    const totalDays =
+      this.calendar.month.numberOfDays + totalLastMonthFinalDays;
+    const monthList = Array.from({ length: totalDays });
 
-    for(let i = totalLastMonthFinalDays; i < totalDays; i++) {
-      monthList[i] = this.calendar.month.getDay(i + 1 - totalLastMonthFinalDays)
+    for (let i = totalLastMonthFinalDays; i < totalDays; i++) {
+      monthList[i] = this.calendar.month.getDay(
+        i + 1 - totalLastMonthFinalDays
+      );
     }
 
     return monthList;
   }
 
   updateMonthDays() {
-    return (this.getMonthDaysGrid() as unknown as Day[]).map( day => {
-      return day ? day : ''
-    })
+    // return (this.getMonthDaysGrid() as unknown as Day[]).map( day => {
+    //   return day ? day : ''
+    // })
+    setTimeout(() => {
+      const monthDaysElement = document.getElementById("month-days");
+      monthDaysElement.innerHTML = "";
+
+      (this.getMonthDaysGrid() as Day[]).forEach((day) => {
+        const btnDay = document.createElement("button");
+        btnDay.className = "month-day";
+        btnDay.textContent = day && day.date ? day.date : "";
+        btnDay.addEventListener("click", (e) => this.selectDay(btnDay, day));
+        btnDay.setAttribute("aria-label", day ? day.format(this.format) : "");
+
+        if (day) {
+          if (day.monthNumber === this.calendar.month.number) {
+            btnDay.classList.add("current");
+          }
+
+          if (this.isSelectedDate(day)) {
+            btnDay.classList.add("selected");
+            this.selectedDayElement = btnDay;
+          }
+        }
+
+        if (monthDaysElement) {
+          monthDaysElement.appendChild(btnDay);
+        }
+      });
+    });
+  }
+
+  selectDay(el, day) {
+    if (!day) return;
+    console.log(day, this.date);
+
+    if (day.isEqualTo(this.date)) return;
+
+    this.date = day;
+
+    if (day.monthNumber !== this.calendar.month.number) {
+      this.previousMonth();
+    } else {
+      el.classList.add("selected");
+      this.selectedDayElement.classList.remove("selected");
+      this.selectedDayElement = el;
+    }
+
+    this.toggle();
+    this.updateMonthDays();
   }
 
   isSelectedDate(date) {
-    return date.date === this.date.date &&
+    return (
+      date.date === this.date.date &&
       date.monthNumber === this.date.monthNumber &&
-      date.year === this.date.year;
+      date.year === this.date.year
+    );
   }
 }
